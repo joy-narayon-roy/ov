58b653a313787c011b04a52a349d32f6b8f54591
joyroy261216@gmail.com <demoyeti420@gmail.com>
2024-07-05 18:24:06 +0600

    Clean up

--- old_data_collector/vpn_connected/__init__.py
+++
@@ -1,3 +0,0 @@
-from .main import is_connected_to_vpn
-
-# ["is_connected_to_vpn"]
--- python/verify_task/files.sh
+++
@@ -1,4 +0,0 @@
-html=$(find ./html -type f | wc -l)
-logs=$(find ./log -type f | wc -l)
-echo "HTML :" $html 
-echo "Errors :" $logs
