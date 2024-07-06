859ea02de4366bf60d44cfac6fbed3b7bcdc3bab
joyroy261216@gmail.com <demoyeti420@gmail.com>
2024-07-06 12:45:52 +0600

    fix copy_html_to_store.sh

--- nodejs/verify_reg_task/client/copy_html_to_store.sh
+++ nodejs/verify_reg_task/client/copy_html_to_store.sh
@@ -5,5 +5,5 @@ html=$(ls -1 ./html | wc -l)
 echo "Copy HTML : "$html
 
 find html/ -name '*.html' -print0 | xargs -0 cp -t store/html
-nowhtml=$(ls -1 ./store/html | wc -l)
+newhtml=$(ls -1 ./store/html | wc -l)
 echo "Now Store HTML :" $newhtml
