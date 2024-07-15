1757024d18c0ab694aa39d39368af065f868ec26
joyroy261216@gmail.com <demoyeti420@gmail.com>
2024-07-15 22:08:36 +0600

    wga155

--- python/data_collected/server.py
+++ python/data_collected/server.py
@@ -8,10 +8,24 @@ app = Flask(__name__)
 def insert_reg(reg):
     conn = sqlite3.connect("./db/regs.db")
     curs = conn.cursor()
-    curs.execute('INSERT OR IGNORE INTO Regs(reg,collected) VALUES (?,0)',(reg,))
+    curs.execute(
+        'INSERT OR IGNORE INTO Regs(reg,collected) VALUES (?,0)', (reg,))
     conn.commit()
     conn.close()
     return str(reg), 200
+
+
+@app.route("/api/all")
+def get_all_data():
+    limit = request.args.get('limit', default=10, type=int) or 10
+    page = request.args.get('page', default=0, type=int) or 0
+    offset = page * limit
+    conn = sqlite3.connect("./db/regs.db")
+    curs = conn.cursor()
+    data = curs.execute(
+        f"SELECT * FROM Regs WHERE data NOT NULL ORDER BY reg ASC LIMIT {limit} OFFSET {offset}").fetchall()
+    data = list(map(lambda d:(d[0],json.loads(d[1])),data))
+    return jsonify(data)
 
 
 @app.route('/test/api/student/login', methods=['GET', 'POST'])
@@ -35,4 +49,4 @@ def test_server():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0", port=8200)
+    app.run(debug=True, host="0.0.0.0", port=8200)
